6871fdd8197e4e7b55e91fcb011638433f1c00ac
tpyoung <tylerparkeyoung@gmail.com>
2018-01-11 17:03:34 -0800

    event listener for page load

--- src/Graph.js
+++ src/Graph.js
@@ -5,49 +5,47 @@ class Graph extends Component {
 	constructor(){
 		super();
 		this.state ={
-			btcArray: []
+			btcArray: [],
+			startDate: '2010-09-01',
+			endDate: '2017-12-19',
 		};
 	}
 
+	componentDidMount() {
+    window.addEventListener('load', () => this.getRequest());
+ 	}
+
 	getRequest() {
-	// let url = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${this.startDate}}&end=${this.endDate}`
-	  let btcArray = []
-	  let url = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2010-09-01&end=2017-12-19';
+	  let btcArray = [];
+		let url = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${this.state.startDate}&end=${this.state.endDate}`;
 	  fetch(url, {'headers': {'Accept': 'application/json'}})
 	  .then(res  => res.json())
 	  .then(res => Object.values(res.bpi).forEach((value) => {
 	  	btcArray.push({"BTC": value});
 	  }))
 	  .then(res => this.setState({
-	  	btcArray: btcArray
-	  	// arrayLength: Object.keys(res.bpi).length, 
+	  	btcArray
 	  	// dateValue: Object.keys(res.bpi)[10], 
-	  	// currencyValue: Object.values(res.bpi)[10]
 	  }))
-	  .catch(err => console.log('fetch error:', err))
-	  
+	  .catch(err => console.log('fetch error:', err));
 	}
 
 	render(){
 		return (
 			<div>
 			<h1>cryptoTears</h1>
-			<button onClick={() => this.getRequest()}>click</button>
-			<button onClick={() => console.log(this.state.btcArray)}>click2</button>
 				<Stream
 				      data={this.state.btcArray}
-				      keys={['BTC', 'ETH']}
+				      keys={['BTC']}
 				      enableGridX={false}
-				      
 				      width={screen.width}
 				      height={400}
 				      margin={{
 				          "top": 50,
-				          "right": 0,
+				          "right": 20,
 				          "bottom": 0,
 				          "left": 0
 				      }}
-				      axisBottom={false}
 				      offsetType="wiggle"
 				      colors="set3"
 				      fillOpacity={0.85}
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Graph from './Graph'
 
+
 ReactDOM.render(<Graph />, document.getElementById('root'));
 
